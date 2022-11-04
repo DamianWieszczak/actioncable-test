b896cb6fef95ddfcdf02bed86fc1de9630d100db
DamianWieszczak <damian.wieszczak@gmail.com>
2022-11-04 18:46:59 +0100

    Add users to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+User.create(email: 'johndoe@example.com', password: 'johntester')
+User.create(email: 'andrewaverage@example.com', password: 'andrewtester')
